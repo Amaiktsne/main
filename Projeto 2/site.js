1da4b24f06e5bf3b02b6d9d839c53383c14d64ae
Mikaela <amaiktsne@gmail.com>
2023-06-13 17:05:17 -0300

    update

--- Projeto 2/site.js
+++ Projeto 2/site.js
@@ -1,39 +1,18 @@
 window.addEventListener("DOMContentLoaded", function() {
 
-//     var form = document.querySelector("form");
-//     var nameInput = document.getElementById("name");
-//     var emailInput = document.getElementById("email");
-//     var messageInput = document.getElementById("message");
-
-
-//     form.addEventListener("submit", function(event) {
-// event.preventDefault(); 
-
-
-//     var name = nameInput.value;
-//     var email = emailInput.value;
-//     var message = messageInput.value;
-
-
-//     console.log("Nome:", name);
-//     console.log("Email:", email);
-//     console.log("Mensagem:", message);
-
-
-//     nameInput.value = "";
-//     emailInput.value = "";
-//     messageInput.value = "";
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('#carrossel_imagens').slick({
-//         autoplay: true,
-//         arrows: false,
-//         autoplaySpeed: 1000,
-//     });
-
     $('.menu_hamburger').click(function(){
         $('nav').slideToggle();
     })
 })
+
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();  
+
+    const emailInput = document.getElementById('emailInput');
+    const email = emailInput.value;
+    alert('E-mail enviado: ' + email);
+
+    window.location.href = 'mailto:amaiktsne@gmail.com';
+});
+
