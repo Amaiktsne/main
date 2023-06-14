994b15a3117ea2a0609b975f163116015a140b7b
Mikaela <amaiktsne@gmail.com>
2023-06-14 13:33:27 -0300

    update

--- Projeto 2/site.js
+++ Projeto 2/site.js
@@ -5,14 +5,4 @@ window.addEventListener("DOMContentLoaded", function() {
     })
 })
 
-const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', function(event) {
-    event.preventDefault();  
-
-    const emailInput = document.getElementById('emailInput');
-    const email = emailInput.value;
-    alert('E-mail enviado: ' + email);
-
-    window.location.href = 'mailto:amaiktsne@gmail.com';
-});
 
